08782f6044b81fa96b836e406436d7e5472435a3
Gregory <grisha101202@yanex.ru>
2022-12-03 21:14:24 +0300

    add consultant

---
+++ carsharing/src/main/java/com/example/carsharing/controller/ConsultantController.java
@@ -0,0 +1,65 @@
+package com.example.carsharing.controller;
+
+import com.example.carsharing.dto.EmployeeDto;
+import com.example.carsharing.models.Consultant;
+import com.example.carsharing.service.ConsultantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/consultant")
+public class ConsultantController {
+    private final ConsultantService consultantService;
+
+    @Autowired
+    public ConsultantController(ConsultantService techSpecialistService) {
+        this.consultantService = techSpecialistService;
+    }
+
+    @GetMapping
+    public String showConsultantPage(Model model) {
+        model.addAttribute("list", consultantService.getAll());
+        return "consultant";
+    }
+
+    @GetMapping("/showNewConsultantForm")
+    public String showNewConsultantForm(Model model) {
+        model.addAttribute("consultant", new EmployeeDto());
+        return "new_consultant";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute(value = "consultant") EmployeeDto employeeDto) {
+        consultantService.save(employeeDto);
+        return "redirect:/consultant/showNewConsultantForm?success";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") long id) {
+        consultantService.deleteById(id);
+        return "redirect:/consultant";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showNewTechSpecialistForm(@PathVariable(value = "id") long id, Model model) {
+        Consultant consultant = consultantService.findById(id);
+        EmployeeDto employeeDto = new EmployeeDto(
+                consultant.getId(),
+                consultant.getFirstName(),
+                consultant.getLastName(),
+                consultant.getPassportData().substring(0, 4),
+                consultant.getPassportData().substring(4),
+                consultant.getEmploymentContract()
+        );
+        model.addAttribute("consultant", employeeDto);
+        return "update_consultant";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute(value = "consultant") EmployeeDto employeeDto) {
+        consultantService.update(employeeDto);
+        return "redirect:/consultant/showFormForUpdate/" + employeeDto.getId() + "?success";
+    }
+}
---
+++ carsharing/src/main/java/com/example/carsharing/service/ConsultantService.java
@@ -0,0 +1,50 @@
+package com.example.carsharing.service;
+
+import com.example.carsharing.dto.EmployeeDto;
+import com.example.carsharing.models.Consultant;
+import com.example.carsharing.repository.ConsultantRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConsultantService {
+    private final ConsultantRepository consultantRepository;
+
+    public ConsultantService(ConsultantRepository consultantRepository) {
+        this.consultantRepository = consultantRepository;
+    }
+
+    public Consultant save(EmployeeDto employeeDto) {
+        Consultant consultant = new Consultant(
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getPasswordSeries() + employeeDto.getPasswordNumber(),
+                employeeDto.getEmploymentContract()
+        );
+        return consultantRepository.save(consultant);
+    }
+
+    public List<Consultant> getAll() {
+        return consultantRepository.findAll();
+    }
+
+    public void deleteById(long id) {
+        consultantRepository.deleteById(id);
+    }
+
+    public Consultant findById(long id) {
+        return consultantRepository.findById(id);
+    }
+
+    public Consultant update(EmployeeDto employeeDto) {
+        Consultant consultant = new Consultant(
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getPasswordSeries() + employeeDto.getPasswordNumber(),
+                employeeDto.getEmploymentContract()
+        );
+        consultant.setId(employeeDto.getId());
+        return consultantRepository.save(consultant);
+    }
+}
